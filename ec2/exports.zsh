b3861734e996ef5a6378eef3b3d03be8bab378cd
John Stallings <jdstallings@gmail.com>
2011-11-17 08:44:47 -0800

    aws tools installed using brew

---
+++ ec2/exports.zsh
@@ -0,0 +1,4 @@
+export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.4.4.1/jars"
+export AWS_CLOUDFORMATION_HOME="/usr/local/Cellar/aws-cfn-tools/1.0.9/jars"
+
